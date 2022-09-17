6ebf5e8e85ebf12886eada773a75f3284e0dd15f
Marco Rossi <developer@marco-rossi.com>
2022-09-17 14:35:49 +0200

    Remove generation of dotnet runtime config

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,10 +2,7 @@ try:
     from importlib.resources import files as resources_files
 except ImportError:
     from importlib_resources import files as resources_files
-import json
-import os
 import sys
-import tempfile
 
 import clr_loader
 
@@ -22,17 +19,4 @@ if sys.platform == 'win32':
     clr_loader.get_netfx()
 if sys.platform != 'win32':
     clr_loader.get_mono()
-runtime_config = {
-    'runtimeOptions': {
-        'tfm': 'netcoreapp6.0',
-        'framework': {
-            'name': 'Microsoft.NETCore.App',
-            'version': '6.0.0',
-        },
-    },
-}
-with tempfile.TemporaryDirectory() as tmpdir:
-    runtime_config_file = os.path.join(tmpdir, 'runtime_config.json')
-    with open(runtime_config_file, 'w') as file:
-        file.write(json.dumps(runtime_config))
-    clr_loader.get_coreclr(runtime_config_file)
+clr_loader.get_coreclr()
